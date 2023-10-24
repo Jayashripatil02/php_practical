a8b2969450605e0e578c15c1abaf96229ce9a5b6
Jayashri patil <114655841+Jayashripatil02@users.noreply.github.com>
2023-10-24 15:01:26 +0530

    Add files via upload

---
+++ array.php
@@ -0,0 +1,11 @@
+<?php
+$cars=array("BMW","Thar","RRR");
+print_r($cars);
+echo "<br> We Purchase $cars[1] car,<br>";
+$cars[]="Suzuki";
+print_r($cars);
+echo "<br>";
+$ages=array("jayu"=>18,"mayu"=>19,"vaishu"=>21);
+print_r($ages);
+echo "<br>"
+?>
